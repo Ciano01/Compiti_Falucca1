14d507382e9b032386d3f4281ba8de54a939bdc4
Mattia <Mattia@DESKTOP-1DT4BRN>
2018-09-18 16:05:47 +0200

    m

---
+++ CompitiMigni/ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10101;
+            Console.WriteLine($"il valore di a è {a}");
+            short s = (short)a;
+            Console.WriteLine($"il valore di s è {s}");
+        }
+    }
+}
