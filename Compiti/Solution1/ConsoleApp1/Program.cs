60e32a038c5a9a956ab9021aadf30edfc0cfa1f4
Mattia <Mattia@DESKTOP-1DT4BRN>
2018-09-25 18:23:11 +0200

    Compiti per il 26\09\2018

---
+++ Compiti/Solution1/ConsoleApp1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Write();
+            Console.Write("Salve");
+            Task t = new Task(Write);
+            t.Start();
+            int i = 100;
+            while (i > 0)
+            {
+                Console.Write("Mhanz");
+                i = i - 1;
+            }
+            Console.ReadLine();
+        }
+                public static void Write()
+                {
+                    int i = 10;
+                    while (i >= 0)
+                    {
+                        Console.WriteLine(i);
+                        i--;
+                    }
+                }
+    }
+}
